30b0ca7dcc9ce547ebbcb790868f5478082d2617
MattChaps <34246991+MattChaps@users.noreply.github.com>
2020-07-06 01:03:58 +0700

    Move include directives

--- gameoflife/gameoflife.c
+++ gameoflife/gameoflife.c
@@ -1,9 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
-#include<ctype.h>
-
 #include"gol.h"
 
 int main (int argc, char *argv[]) 
--- gameoflife/gol.h
+++ gameoflife/gol.h
@@ -1,6 +1,12 @@
 #ifndef GOL_H
 #define GOL_H
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+#include<ctype.h>
+
 struct universe {
 	int cells;
 	int chars;
